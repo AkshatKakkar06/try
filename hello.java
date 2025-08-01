9a7dc13362fac4589ee864a48ba06a8a83aef458
AkshatKakkar06 <akshatkakkar06@gmail.com>
2025-08-02 04:46:50 +0530

    solved some errors

--- hello.java
+++ hello.java
@@ -1,7 +1,9 @@
 class NotHello {
     public static void main(String[] args) {
         System.out.println("This is not hello.java");
-public class Hello {
+    }
+}    
+public class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
